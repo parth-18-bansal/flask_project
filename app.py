dadecf43e43e5ad2bbcbb55cf1826ae9d587e3e3
parth-18-bansal <parthbansalcoding@gmail.com>
2024-11-14 22:19:55 +0530

    integrating the html page with the flask

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask,render_template
+
 
 #wsgi application
 app = Flask(__name__)               #it creates the instance of the Flask class. it will be the wsgi
@@ -6,6 +7,16 @@ app = Flask(__name__)               #it creates the instance of the Flask class.
 @app.route("/")
 def welcome():
     return "<html><H1>welcome to the website</H1></html>"
+
+# here when we go to the /index path, we will redirect to the index.html page.
+# here render_template will search for the file in the template folder
+@app.route("/index")
+def index_page():
+    return render_template("index.html")
+
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
